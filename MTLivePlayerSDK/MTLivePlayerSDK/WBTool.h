aae317260109a2b469434e41e11a938281844b41
wangbin <bin.wang@mtime.com>
2017-02-15 14:12:26 +0800

    framework init

---
+++ MTLivePlayerSDK/MTLivePlayerSDK/WBTool.h
@@ -0,0 +1,15 @@
+//
+//  WBTool.h
+//  WBDynamic
+//
+//  Created by wangbin on 2017/1/10.
+//  Copyright © 2017年 wangbin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WBTool : NSObject
+
++ (int)sumWithNumerOne:(int)numerOne numberTwo:(int)numberTwo;
+
+@end
---
+++ MTLivePlayerSDK/MTLivePlayerSDK/WBTool.m
@@ -0,0 +1,20 @@
+
+//
+//  WBTool.m
+//  WBDynamic
+//
+//  Created by wangbin on 2017/1/10.
+//  Copyright © 2017年 wangbin. All rights reserved.
+//
+
+#import "WBTool.h"
+
+@implementation WBTool
+
+
++ (int)sumWithNumerOne:(int)numerOne numberTwo:(int)numberTwo{
+
+    return numberTwo + numerOne;
+
+}
+@end
